df6d6e7a41edd39c83f07ca8b4378d3616d9d930
vivisvargas13 <vimar95@gmail.com>
2025-01-28 17:20:02 -0600

    category

---
+++ Northwind/BackEnd/Services/Implementaciones/CategoryService.cs
@@ -0,0 +1,35 @@
+﻿using BackEnd.Services.Interfaces;
+using DAL.Interfaces;
+using Entities.Entities;
+
+namespace BackEnd.Services.Implementaciones
+{
+    public class CategoryService : ICategoryService
+    {
+        private ICategoryDAL _categoryDAL;
+
+        public CategoryService(ICategoryDAL categoryDAL)
+        {
+            _categoryDAL = categoryDAL;
+        }
+        public void AddCategory(Category category)
+        {
+            _categoryDAL.AddCategory(category);
+        }
+
+        public void DeleyeCategory(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Category> GetCategories()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateCategory(Category category)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Northwind/BackEnd/Services/Interfaces/ICategoryService.cs
@@ -0,0 +1,12 @@
+﻿using Entities.Entities;
+
+namespace BackEnd.Services.Interfaces
+{
+    public interface ICategoryService
+    {
+        void AddCategory(Category category);
+        void UpdateCategory(Category category);
+        void DeleyeCategory(int id);
+        List<Category> GetCategories();
+    }
+}
